922fa868da879e2f7f5f04b99ec4d97bdd546b70
Arjun M <arjun@statwig.com>
2026-08-20 20:47:35 +0530

    New push

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
-  nitro: { preset: "vercel" },
+  // nitro: { preset: "vercel" },
   vite: {
     assetsInclude: ["**/*.geojson"],
   },
